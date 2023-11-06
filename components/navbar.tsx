ba4c58c2f945d1a6cbe648b8aa7dd69b13ea2f3d
ASCA1ON <akash2468.ab@gmail.com>
2023-11-06 21:04:22 +0530

    code formatting

--- components/navbar.tsx
+++ components/navbar.tsx
@@ -5,32 +5,31 @@ import { MainNav } from "./main-nav";
 import StoreSwitcher from "./store-switcher";
 import { ModeToggle } from "./theme-toggle";
 
-
 const Navbar = async () => {
-    const { userId } = auth();
-  
-    if (!userId) {
-      redirect('/sign-in');
-    }
-  
-    const stores = await prismadb.store.findMany({
-      where: {
-        userId,
-      }
-    });
-  
-    return ( 
-      <div className="border-b">
-        <div className="flex h-16 items-center px-4">
+  const { userId } = auth();
+
+  if (!userId) {
+    redirect("/sign-in");
+  }
+
+  const stores = await prismadb.store.findMany({
+    where: {
+      userId,
+    },
+  });
+
+  return (
+    <div className="border-b">
+      <div className="flex h-16 items-center px-4">
         <StoreSwitcher items={stores} />
-          <MainNav className="mx-6" /> 
-          <div className="ml-auto flex items-center space-x-4">
-            <ModeToggle/>
-            <UserButton afterSignOutUrl="/" />
-          </div>
+        <MainNav className="mx-6" />
+        <div className="ml-auto flex items-center space-x-4">
+          <ModeToggle />
+          <UserButton afterSignOutUrl="/" />
         </div>
       </div>
-    );
-  };
-   
-  export default Navbar;
+    </div>
+  );
+};
+
+export default Navbar;
--- providers/modal-provider.tsx
+++ providers/modal-provider.tsx
@@ -1,23 +1,22 @@
-'use client'
+"use client";
 
 import { useEffect, useState } from "react";
 import { StoreModal } from "@/components/modals/store-modal";
 
-
 export const ModalProvider = () => {
-    const [isMounted, setIsMounted] = useState(false);
-  
-    useEffect(() => {
-      setIsMounted(true);
-    }, []);
-  
-    if (!isMounted) {
-      return null;  //? to only return on client side (hydration error fix)
-    }
-  
-    return (
-      <>
-        <StoreModal />
-      </>
-    );
+  const [isMounted, setIsMounted] = useState(false);
+
+  useEffect(() => {
+    setIsMounted(true);
+  }, []);
+
+  if (!isMounted) {
+    return null; //? to only return on client side (hydration error fix)
   }
+
+  return (
+    <>
+      <StoreModal />
+    </>
+  );
+};
